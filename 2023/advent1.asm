1050c6e423538a5a1c2be42e3fda669e1b277e93
mic- <micol972@gmail.com>
2023-12-02 22:22:23 +0100

    Simplified the file parsing and made it work with arbitrarily long lines

--- 2023/advent1.asm
+++ 2023/advent1.asm
@@ -19,20 +19,17 @@ includelib \masm32\lib\user32.lib
 
 wsprintf equ <wsprintfA>
 
+INPUT_BUFFER_SIZE equ 1024
+
 .data?
-    result           dd ?
-    hInputFile       dd ?
-    hConsole         dd ?
-    bytesRead        dd ?
-    szBuffer         db 256 dup (?)
-    lineBuffer       db 1024 dup (?)
-    szLineView       db 513 dup (?)
-    lineBufferOffset dd ?
-    lineBufferLength dd ?
-    activeLineBuffer dd ?
+    hInputFile        dd ?
+    hConsole          dd ?
+    bytesRead         dd ?
+    szBuffer          db 256 dup (?)
+    inputBuffer       db INPUT_BUFFER_SIZE dup (?)
 
 .data
-szFormatSum db 'Sum of calibration values: %d',13,10,0
+    szFormatSum db 'Sum of calibration values: %d',13,10,0
 
 .code
 
@@ -56,8 +53,7 @@ ENDM
 
 get_filename_from_commandline PROTO STDCALL
 open_file PROTO STDCALL :DWORD
-read_line_from_file PROTO STDCALL :DWORD
-parse_line PROTO STDCALL :DWORD
+parse_file PROTO STDCALL :DWORD
 
 ;----------------------------------------------------------------------------------------
 
@@ -69,27 +65,17 @@ start:
     invoke open_file, eax
     mov [hInputFile], eax
 
-    mov [result],0
-    mov [lineBufferOffset],0
-    mov [lineBufferLength], 0
-    mov [activeLineBuffer], OFFSET lineBuffer
-    xor edi,edi
-loop_lines:
-    invoke read_line_from_file, hInputFile
-    test eax,eax
-    jz end_of_file
-    invoke parse_line, eax
-    add [result],eax
-    jmp loop_lines
-end_of_file:
+    invoke parse_file, hInputFile
+    push eax
     invoke CloseHandle, hInputFile
+    pop eax
 
     pusha
-    invoke wsprintf, ADDR szBuffer, ADDR szFormatSum, result
+    invoke wsprintf, ADDR szBuffer, ADDR szFormatSum, eax
     invoke WriteConsole, hConsole, ADDR szBuffer, eax, ADDR bytesRead, NULL
     popa
 
-invoke ExitProcess, 0
+    invoke ExitProcess, 0
 
 
 ; Get the name of the input file from the command line
@@ -132,91 +118,60 @@ open_file PROC lpszFilename:DWORD
 open_file ENDP
 
 
-; Read one line from the given file
-; @param hFile A handle to the file to read from
-; @return A pointer to a zero-terminated string containing the line read, or NULL
+; Parse all lines in the given file
+; @param hFile The file handle
+; @return The sum of all rows in the file, where the value of each row
+;         is first*10+last, with 'first' and 'last' being the first and
+;         last digits found on that line
 ;
-read_line_from_file PROC hFile:DWORD
-    mov esi,[activeLineBuffer]
-    lea edi,szLineView
-    mov ecx,[lineBufferOffset]
-    mov edx,[lineBufferLength]
-parse_buffer:
-    cmp ecx,edx
-    jae buffer_more_data
-    mov al,[esi + ecx]
-    inc ecx
-    cmp al,13
-    je newline
-    cmp al,10
-    je newline
-    mov [edi],al
-    inc edi
-    jmp parse_buffer
-newline:
-    mov [lineBufferOffset],ecx
-    mov byte ptr [edi],0
-    lea eax,szLineView
-    ret
-buffer_more_data:
-    mov ebx,512
-    mov eax,[activeLineBuffer]
-    cmp eax,OFFSET lineBuffer
-    je @F
-    neg ebx
-@@:
-    add eax,ebx
-    mov [activeLineBuffer],eax
-    invoke ReadFile, hFile, eax, 512, ADDR lineBufferLength, NULL
-    mov esi,[activeLineBuffer]
-    xor ecx,ecx
-    mov edx,[lineBufferLength]
-    test edx,edx
-    jne parse_buffer
-    cmp edi,OFFSET szLineView
-    ja @F
-    xor eax,eax
-    ret
-@@:
-    mov byte ptr [edi],0
-    lea eax,szLineView
-    ret
-read_line_from_file ENDP
-
-
-; Parse the given line
-; @param lpszLine A pointer to a zero-terminated string containing the line to parse
-; @return The value first*10+last, where 'first' and 'last' is the first and last digits found on the line
-;
-parse_line PROC lpszLine:DWORD
+parse_file PROC hFile:DWORD
+    LOCAL result:DWORD
     xor ebx,ebx
-    xor ecx,ecx
+    mov [result],ebx
     xor edx,edx
-    mov eax,[lpszLine]
-search_digits:
-    mov bl,[eax]
-    test ebx,ebx
-    jz end_of_string
+    xor esi,esi
+    xor edi,edi
+parse_input:
+    lea eax,inputBuffer
+    xor ecx,ecx
+parse_line:
+    cmp esi,edi
+    jb have_data
+    pusha
+    invoke ReadFile, hFile, eax, INPUT_BUFFER_SIZE, ADDR bytesRead, NULL
+    popa
+    xor esi,esi
+    mov edi,[bytesRead]
+    cmp edi,0
+    jle end_of_line
+have_data:
+    mov bl,[eax + esi]
+    inc esi
+    cmp bl,13
+    je end_of_line
+    cmp bl,10
+    je end_of_line
     cmp bl,'0'
-    jb @F
+    jb parse_line
     cmp bl,'9'
-    ja @F
+    ja parse_line
     test ecx,ecx
     cmovz ecx,ebx
     mov edx,ebx
-@@:
-    inc eax
-    jmp search_digits
-end_of_string:
-    xor eax,eax
+    jmp parse_line
+end_of_line:
     test ecx,ecx
     jz @F
     sub ecx,'0'
     sub edx,'0'
     lea eax,[ecx*4 + ecx]
     lea eax,[eax*2 + edx]
+    add [result],eax
 @@:
+    test edi,edi
+    jne parse_input
+    mov eax,[result]
     ret
-parse_line ENDP
+parse_file ENDP
 
 END start
