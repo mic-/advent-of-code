1f4325224eb0a95544389c9cd52b5fb1bd06b3cf
mic- <micol972@gmail.com>
2024-12-01 09:33:18 +0100

    Fixed some typos

--- 2024/day-1/Advent1_2.groovy
+++ 2024/day-1/Advent1_2.groovy
@@ -1,4 +1,4 @@
-// Advent of Code 2023 - Day 1: Historian Hysteria, part 2
+// Advent of Code 2024 - Day 1: Historian Hysteria, part 2
 // Mic, 2024
 
 static void main(String[] args) {
@@ -10,7 +10,7 @@ static void main(String[] args) {
     def input = new File(args[0]).readLines()
     def lists = [[], []]
     input.each {
-        def values = it.split().collect {it.toInteger() }
+        def values = it.split().collect { it.toInteger() }
         lists[0] += values[0]
         lists[1] += values[1]
     }
