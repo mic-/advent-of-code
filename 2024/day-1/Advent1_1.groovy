710ac719f293f7a81b2738ea9663afd9d7c911bc
mic- <micol972@gmail.com>
2024-12-01 09:25:56 +0100

    2024, day 1

---
+++ 2024/day-1/Advent1_1.groovy
@@ -0,0 +1,20 @@
+// Advent of Code 2023 - Day 1: Historian Hysteria, part 1
+// Mic, 2024
+
+static void main(String[] args) {
+  if (!args.length) {
+    println "Error: no input file specified"
+    return
+  }
+
+  def input = new File(args[0]).readLines()
+  def lists = [[], []]
+  input.each {
+    def values = it.split().collect {it.toInteger() }
+    lists[0] += values[0]
+    lists[1] += values[1]
+  }
+  lists.each { it.sort() }
+
+  print("Total distance between lists: " + lists[0].withIndex().collect { value, index -> Math.abs(lists[1][index] - value) }.sum())
+}
---
+++ 2024/day-1/Advent1_2.groovy
@@ -0,0 +1,19 @@
+// Advent of Code 2023 - Day 1: Historian Hysteria, part 2
+// Mic, 2024
+
+static void main(String[] args) {
+    if (!args.length) {
+        println "Error: no input file specified"
+        return
+    }
+
+    def input = new File(args[0]).readLines()
+    def lists = [[], []]
+    input.each {
+        def values = it.split().collect {it.toInteger() }
+        lists[0] += values[0]
+        lists[1] += values[1]
+    }
+
+    print("Similarity score: " + lists[0].collect { value -> value*lists[1].count(value) }.sum())
+}
